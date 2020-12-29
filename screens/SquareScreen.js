c1ad83c1149ef662c754f54993365cae3b496c6b
GabrielSavian <gabriel@amplimed.com.br>
2020-12-29 14:18:44 -0300

    finished reducer

--- screens/SquareScreen.js
+++ screens/SquareScreen.js
@@ -26,22 +26,23 @@ const reducer = (state, action) => {
 const SquareScreen = () => {
 
     const [state, dispatch] = useReducer(reducer, {red: 0, blue: 0, green: 0});
+    const  {red, green, blue} = state;
     return(
         <View>
             <ColourCounter 
-                onIncrease={() => }
-                onDecrease={() => } 
+                onIncrease={() => dispatch({colourToChange: 'red', amount: COLOUR_INCREMENT})}
+                onDecrease={() => dispatch({colourToChange: 'red', amount: -1 * COLOUR_INCREMENT})} 
                 colour="Red" 
             />
 
             <ColourCounter 
-                onIncrease={() => } 
-                onDecrease={() => } 
+                onIncrease={() => dispatch({colourToChange: 'blue', amount: COLOUR_INCREMENT})}
+                onDecrease={() => dispatch({colourToChange: 'blue', amount: -1 * COLOUR_INCREMENT})} 
                 colour="Blue" 
             />
             <ColourCounter 
-                onIncrease={() => } 
-                onDecrease={() => } 
+                onIncrease={() => dispatch({colourToChange: 'green', amount: COLOUR_INCREMENT})}
+                onDecrease={() => dispatch({colourToChange: 'green', amount: -1 * COLOUR_INCREMENT})} 
                 colour="Green" 
             />
             <View
