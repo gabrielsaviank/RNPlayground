a6711f693f12af57924cdc831cd73b835c0e5979
GabrielSavian <gabriel@amplimed.com.br>
2021-01-11 08:59:48 -0300

    Redux Convention syntax

--- screens/SquareScreen.js
+++ screens/SquareScreen.js
@@ -8,50 +8,51 @@ import ColourCounter from '../components/ColourCounter';
 
 const COLOUR_INCREMENT = 15;
 
+//REDUCER
 const reducer = (state, action) => {
     //  state === {red: number, gren: number, blue: number}
-    // action === { colourToChange: 'red' || 'green' || 'blue', amount: 15 || -15 } 
-    switch (action.colourToChange) {
-        case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0  
+    // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 } 
+    switch (action.type) {
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0  
                 ? state
-                : { ...state, red: state.red + action.amount }; 
+                : { ...state, red: state.red + action.payload }; 
             
-        case 'blue':
-            if(state.blue + action.amount > 255 || state.blue + action.amount < 0 ) {
+        case 'change_blue':
+            if(state.blue + action.payload > 255 || state.blue + action.payload < 0 ) {
                 return state;
             }
-            return { ...state, blue: state.blue + action.amount }; 
-        case 'green': 
-            if(state.green + action.amount > 255 || state.green + action.amount < 0 ) {
+            return { ...state, blue: state.blue + action.payload }; 
+        case 'change_green': 
+            if(state.green + action.payload > 255 || state.green + action.payload < 0 ) {
                 return state;
             }
-            return { ...state, green: state.green + action.amount }; 
+            return { ...state, green: state.green + action.payload }; 
         default:
             return state;
     }
 };
 
 const SquareScreen = () => {
-    // reducer
+    
     const [state, dispatch] = useReducer(reducer, {red: 0, blue: 0, green: 0});
     const  {red, green, blue} = state;
     return(
         <View>
             <ColourCounter 
-                onIncrease={() => dispatch({colourToChange: 'red', amount: COLOUR_INCREMENT})}
-                onDecrease={() => dispatch({colourToChange: 'red', amount: -1 * COLOUR_INCREMENT})} 
+                onIncrease={() => dispatch({type: 'change_red', payload: COLOUR_INCREMENT})}
+                onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOUR_INCREMENT})} 
                 colour="Red" 
             />
 
             <ColourCounter 
-                onIncrease={() => dispatch({colourToChange: 'blue', amount: COLOUR_INCREMENT})}
-                onDecrease={() => dispatch({colourToChange: 'blue', amount: -1 * COLOUR_INCREMENT})} 
+                onIncrease={() => dispatch({type: 'change_blue', payload: COLOUR_INCREMENT})}
+                onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOUR_INCREMENT})} 
                 colour="Blue" 
             />
             <ColourCounter 
-                onIncrease={() => dispatch({colourToChange: 'green', amount: COLOUR_INCREMENT})}
-                onDecrease={() => dispatch({colourToChange: 'green', amount: -1 * COLOUR_INCREMENT})} 
+                onIncrease={() => dispatch({type: 'change_green', payload: COLOUR_INCREMENT})}
+                onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOUR_INCREMENT})} 
                 colour="Green" 
             />
             <View
